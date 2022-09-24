c5c39d3872a56bcf485e3187468d3aad6f04551f
opal06 <luca.scalzini@protonmail.com>
2022-09-24 15:53:13 +0200

    Add download_file()
    Implement function to download files from reMarkable as a zip package

--- rmapi_shim.py
+++ rmapi_shim.py
@@ -24,3 +24,11 @@ def get_files(folder):
     else:
         return False
 
+
+def download_file(file_path, working_dir):
+    # Downloads a file (consisting of a zip file) to a specified directory
+    downloader = subprocess.run(["rmapi", "get", file_path], cwd=working_dir)
+    if downloader.returncode == 0:
+        return True
+    else:
+        return False
