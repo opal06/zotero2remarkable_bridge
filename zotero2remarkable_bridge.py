e5ae404bec86ea8bcbd6268c33e39291c71082fa
opal06 <luca.scalzini@protonmail.com>
2022-09-24 21:55:38 +0200

    Refactor code in main loop to be more modular

--- zotero2remarkable_bridge.py
+++ zotero2remarkable_bridge.py
@@ -8,6 +8,31 @@ from sync_functions import *
 from pdf_functions import add_highlights_simple
 
 
+def push(zot, webdav, folders):
+    sync_items = zot.items(tag="to_sync")
+    print("Found " + str(len(sync_items)) + " elements to sync...")
+    for item in tqdm(sync_items):
+        if webdav:
+            sync_to_rm_webdav(item, zot, webdav, folders)
+        else:
+            sync_to_rm(item, zot, folders)
+    zot.delete_tags("to_sync")
+
+
+def pull(zot, webdav, read_folder):
+    files_list = rmapi.get_files(read_folder)
+    if files_list:
+        for entity in tqdm(files_list):
+            content_id = rmapi.get_metadata(read_folder + entity)["ID"]
+            pdf_name = download_from_rm(entity, read_folder, content_id)
+            add_highlights_simple(entity, content_id, pdf_name)
+            if webdav:
+                zotero_upload_webdav(pdf_name, zot, webdav)
+            else:
+                zotero_upload(pdf_name, zot)
+    else:
+        print("No files to pull found")
+
 
 def main(argv):
     config_path = Path.cwd() / "config.yml"
@@ -29,54 +54,23 @@ def main(argv):
             if arg == "push":
                 # Only sync files from Zotero to reMarkable
                 print("Pushing...")
-                print("Found " + str(len(zot.items(tag="to_sync"))) + " elements to sync...") 
-                for item in tqdm(zot.items(tag="to_sync")):
-                    if webdav: 
-                        sync_to_rm_webdav(item, zot, webdav, folders)
-                    else:
-                        sync_to_rm(item, zot, folders)
-                zot.delete_tags("to_sync")
+                push(zot, webdav, folders)
 
             elif arg == "pull":
                 # Only get files from ReMarkable and upload to Zotero
                 print("Pulling...")
-                files_list = rmapi.get_files(read_folder)
-                if files_list:
-                    for entity in tqdm(files_list):
-                        content_id = rmapi.get_metadata(read_folder + entity)["ID"]
-                        pdf_name = download_from_rm(entity, read_folder, content_id)
-                        add_highlights_simple(entity, content_id, pdf_name)
-                        if webdav:
-                            zotero_upload_webdav(pdf_name, zot, webdav)
-                        else:
-                            zotero_upload(pdf_name, zot)
-                else:
-                    print("No files to pull found")
+                pull(zot, webdav, read_folder)
                 
             elif arg == "both":
                 # Do both
                 print("Do both...")
                 # Upload...
-                for item in tqdm(zot.items(tag="to_sync")):
-                    if webdav:
-                        sync_to_rm_webdav(item, zot, webdav, folders)
-                    else:
-                        sync_to_rm(item, zot, folders)
-                zot.delete_tags("to_sync")        
+                push(zot, webdav, folders)
                 
                 # ...and download, add highlighting and sync to Zotero.
 
-                collection = rm.get_meta_items()
-                files_list = rmapi.get_files(read_folder)
-                for entity in tqdm(files_list):
-                    pdf_name = download_from_rm(entity, read_folder)
-                    content_id = rmapi.get_metadata(read_folder + entity)["ID"]
-                    add_highlights_simple(entity, content_id, pdf_name)
-                    if webdav:
-                        zotero_upload_webdav(pdf_name, zot, webdav)
-                    else:
-                        zotero_upload(pdf_name, zot)
-                            
+                pull(zot, webdav, read_folder)
+
             else:
                 print("Invalid argument")
                 sys.exit()
